3442512d9ba2495c48da48c857a8c5ce22882384
Jeevanthiruchittampalam <113857754+Jeevanthiruchittampalam@users.noreply.github.com>
2023-01-26 23:28:22 -0500

    Add files via upload

---
+++ My_First_Cpp_Game/My_First_Cpp_Game/platform_common.cpp
@@ -0,0 +1,23 @@
+
+struct Button_State {
+	bool is_down;
+	bool changed;
+};
+
+enum {
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_W,
+	BUTTON_S,
+	BUTTON_LEFT,
+	BUTTON_RIGHT,
+	BUTTON_ENTER,
+
+	BUTTON_COUNT, //Supposed to be the last item in the array 
+
+
+};
+
+struct Input {
+	Button_State buttons[BUTTON_COUNT];
+};
